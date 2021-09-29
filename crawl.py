ea26b0c5e6b18f6c694ca8c01eb5ed849d5f9acf
Kevin Grimm <kevin.g.grimm1@gmail.com>
2021-09-29 07:10:18 -0600

    wip: building out  final updates to basic crawler

--- crawl.py
+++ crawl.py
@@ -1,29 +1,87 @@
 import os
 import json
+import logging
+import sys
+from datetime import datetime
+from time import perf_counter, sleep
 
 import requests
 import praw
 
-# "https://www.reddit.com/prefs/apps"
+from config import config
 
-def get_contacted_users():
-    contacted_users = set()
-    for msg in reddit.inbox.messages():
-        if msg.dest.id not in contacted_users:
-            contacted_users.add(msg.dest.id)
-            
-    return contacted_users
+
+# "https://www.reddit.com/prefs/apps"
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+for logger_name in ("praw", "prawcore"):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+# Initialize reddit client
+############################
+client_id     = config.get('CLIENT_ID')
+client_secret = config.get('CLIENT_SECRET')
+username      = config.get('USERNAME')
+password      = config.get('PASSWORD')
+user_agent    = config.get('USER_AGENT')
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    username=username,
+    password=password,
+    user_agent=user_agent
+)
+
+# Load bot data
+####################
+keywords              = load_data('keywords')
+redditors_contacted   = load_data('redditors')
+subreddits            = load_data('subreddits')
+
+# Load config data
+####################
+message_template    = config.get('message_template')
+message_subject     = config.get('message_subject')
+message_limit       = config.get('message_limit')
+general_search_term = config.get('general_search_term')
+
+
+def load_data(file_name, base_dir='data', ending='.txt'):
+    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'r') as f:
+        data = set([l for l in f.read().split("\n")])
+        return data
+
+
+def write_data(data, file_name, base_dir='data', ending='.txt'):
+    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'w') as f:
+        for line in data:
+            f.write(line)
+            f.write('\n')
 
 def message_redditor(redditor, subreddit, subject=subject, message_template=message_template):
     try:
-        message = message_template.replace('__SUBREDDIT', subreddit)
+        message = message_template.replace('__SUBREDDIT__', subreddit)
         redditor.message(subject=subject, message=message)
     except Exception as e:
-        print('Unable to message author: ', str(e))
+        print('Unable to message redditor: ', str(e))
+
+
+def check_api_limits():
+    requests_remaining = reddit.auth.limits.get('remaining')
+    if requests_remaining > 0:
+        return
+    reset_datetime = datetime.fromtimestamp(reddit.auth.limits.get('reset_timestamp'))
+    current_datetime = datetime.now()
+    sleep_seconds = (reset_datetime - current_datetime).seconds
+    sleep(sleep_seconds)
+
 
 def message_submission_commenters(submission, subreddit, contacted_users, message_limit):
     messages_sent = 0
-    
+
     title = submission.title.lower().split(' ')
     title_words = set([word for word in title])
 
@@ -35,7 +93,7 @@ def message_submission_commenters(submission, subreddit, contacted_users, messag
 
             # Message user if not already contacted
             ########################################
-            redditor = c.author
+            redditor = comment.author
             if redditor not in contacted_users:
                 contacted_users.add(redditor.id)
                 message_redditor(redditor, subreddit)
@@ -47,83 +105,49 @@ def message_submission_commenters(submission, subreddit, contacted_users, messag
                     print('Hit message limit - exiting')
                     return messages_sent
 
+SLEEP_TIME = 5
+start_time = perf_counter()
+messages_sent = 0
+for submission in reddit.subreddit("all").search(general_search_term, limit=20):
+    submission.comments.replace_more(limit=None)
 
-
-secret                  = os.getenv('REDDIT_SECRET', None)
-username                = os.getenv('REDDIT_USERNAME', None)
-password                = os.getenv('REDDIT_PASSWORD', None)
-user_agent              = os.getenv('REDDIT_USER_AGENT', None)
-personal_use_script     = os.getenv('REDDIT_PERSONAL_SCRIPT', None)
-
-reddit = praw.Reddit(
-    client_id=personal_use_script,
-    client_secret=secret,
-    user_agent=user_agent,
-    username=username,
-    password=password
-)
-
-# Keywords, post categories, subreddits to include in search
-##############################################################
-keywords = set([
-    'SEC', 'insider', 'insiders', 'transactions', 'trades', 'benchmark', 'performance',
-    'openinsider', 'data', 'analysis', 'benchmark', 'trade', 'transaction', 'stock',
-    'purchase', 'purchases', 'industry', 'market', 'stocks', 'form', 'strategy',
-    'ownership'
-])
-
-post_categories = ['hot', 'rising', 'top', 'gilded']
-
-subreddits = [
-    'investing',
-    'Superstonk',
-    'FluentInFinance',
-    'StockMarket',
-    'wallstreetbets',
-    'options',
-    'stocks',
-    'GME',
-    'ASX_Bets',
-    'amcstock',
-    'market_sentiment'
-]
-
-# Message template, subject
-#############################
-
-subject='Form 4 / SEC Filings Project'
-
-message_template = """
-Hey, I saw your comment in r/__SUBREDDIT__ and wanted to reach out about a related project that I am building to view Form 4 filings. The app will have features like real-time updates, email alerts, views by insiders/ title, companies, industry group/sub-group, and 15+ years of filings.
-
-Wondering if you would be interested in receiving progress updates via email or Reddit (I am targeting a beta release by EOY). Would also be great to get a sense of what other SEC data you would like to access and pain points you might have experienced.
-
-Anyways, figured I would run it by you in case it is of any interest.
-"""
-
-# Main script
-
-contacted_users = get_contacted_users()
-posts_crawled = set()
-message_limit = 100
-
-
-for subreddit in subreddits:
-    print(subreddit)
-    sub = reddit.subreddit(subreddit)
+    # Add subreddit to list if not found
+    ####################################
+    subreddit = submission.subreddit
+    if subreddit not in subreddits:
+        subreddits.add(subreddit)
     
-    # Hot posts
-    ##############
-    for submission in sub.hot(limit=1):
-        messages_sent = message_submission_commenters(submission, subreddit, contacted_users, message_limit)
-        message_limit -= messages_sent        
-        break
+    for comment in submission.comments.list():
+
+        # Add redditor to list if not found
+        ####################################
+        redditor = comment.author
+        redditor_id = redditor.id
+        if redditor not in redditors_contacted:
+            redditors_contacted.add(redditor_id)
+            
+            # Message redditor / increment counters / check rate limits
+            ############################################################
+            message_redditor(redditor, subreddit)
+            check_api_limits()
+            sleep(SLEEP_TIME)
+
+            messages_sent += 1
+            if messages_sent == message_limit:
+
+                # TODO: Write sets to files
+
+                end_time = perf_counter()
+                total_runtime = end_time - start_time
+                total_seconds = round(total_runtime, 2)
+                total_minutes = round((end_time-start_time)/60, 2)
+
+                print(f'Total runtime (seconds): {total_seconds}')
+                print(f'Total runtime (minutes): {total_minutes}')
+                sys.exit("Max number of messages sent - exiting program")
 
 
-## TODO: Review + Incorporate into main script
-# users_contacted = 0
-# message_limit = 100
-
+# TODO: Add subreddit-specific keyword matching
 # for subreddit in subreddits:
 #     print(subreddit)
 #     sub = reddit.subreddit(subreddit)
@@ -132,12 +156,3 @@ for subreddit in subreddits:
 #         title_words = set([word for word in title])
 #         if len(title_words.intersection(keywords)) >= 1:
 #             print(f'Match found in title: {title}')
-            
-
-    
-#     users_contacted += 1
-#     if users_contacted == message_limit:
-#         print(f'Reached message limit of {message_limit} - exiting')
-
-# submission = reddit.submission(url="https://www.reddit.com/r/investing/comments/puhzwr/should_you_follow_insider_transactions_i_analyzed/")
-
