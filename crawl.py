e266af08d0d1bee66600a8aa1a5556d3af75c76b
Kevin Grimm <kevin.g.grimm1@gmail.com>
2021-09-29 20:31:12 -0600

    wip: completed v1 of crawler (in need of refactoring)

--- crawl.py
+++ crawl.py
@@ -11,6 +11,58 @@ import praw
 from config import config
 
 
+#####################################
+def load_data(file_name, base_dir='data', ending='.txt'):
+    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'r') as f:
+        data = set([l for l in f.read().split("\n")])
+        return data
+
+def write_data(data, file_name, base_dir='data', ending='.txt'):
+    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'w') as f:
+        for line in data:
+            f.write(line)
+            f.write('\n')
+
+def message_redditor(redditor, subreddit, message_subject=message_subject, message_template=message_template):
+    try:
+        print('Messaging redditor...')
+        message = message_template.replace('__SUBREDDIT__', subreddit)
+        redditor.message(subject=message_subject, message=message)
+        print('Message sent to redditor')
+        return True
+
+    except Exception as e:       
+
+        # Sleep for time specified in RATELIMIT error
+        ############################################
+        if 'RATELIMIT' in str(e):
+            try:
+                sleep_time = str(e).split('Take a break for ')[-1].split(' ')[0]
+                print(f'Sleeping for {sleep_time} seconds....')
+                sleep(int(sleep_time))
+            except Exception as ex:
+                print(f"Error trying to sleep: {str(ex)}")
+        
+        # General sleep for non whitelist error
+        ################################################
+        elif 'NOT_WHITELISTED_BY_USER_MESSAGE' in str(e):
+            print(f'NOT WHITELISTED ERROR: {str(e)}')
+            sleep(5)
+        return False
+
+def check_api_limits():
+    """Sleeps for time specified in api limits (+ buffer) if no requests are remaining"""
+    from time import sleep
+    requests_remaining = reddit.auth.limits.get('remaining')
+    print(f'API requests remaining: {requests_remaining}')
+    reset_datetime = datetime.fromtimestamp(reddit.auth.limits.get('reset_timestamp'))
+    current_datetime = datetime.now()
+    seconds_until_reset = (reset_datetime - current_datetime).seconds
+    print(f'Seconds until rate limit reset: {seconds_until_reset}')
+    if requests_remaining > 0:
+        return
+    sleep(seconds_until_reset + 10)
+
 # "https://www.reddit.com/prefs/apps"
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
@@ -48,105 +100,177 @@ message_subject     = config.get('message_subject')
 message_limit       = config.get('message_limit')
 general_search_term = config.get('general_search_term')
 
+MESSAGE_SLEEP_TIME  = config.get('message_sleep_time')
+AUTHOR_SLEEP_TIME   = config.get('author_sleep_time')
 
-def load_data(file_name, base_dir='data', ending='.txt'):
-    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'r') as f:
-        data = set([l for l in f.read().split("\n")])
-        return data
+############################################################################################################
 
+# SCRAPER #1 - Pulling latest posts for general searches + updating subreddits // messaging users
+    # TODO:
+    # - Add different, independent keyword searches
+    # - Keyword-specific messages
+    # - Different processes
+        # - Search by keyword (across all of reddit)
+        # - Search by subreddits (predefined)
+    # - Process replied messages
+    # - Convert below processes into functions
 
-def write_data(data, file_name, base_dir='data', ending='.txt'):
-    with open(os.path.join(base_dir, f"{file_name}{ending}"), 'w') as f:
-        for line in data:
-            f.write(line)
-            f.write('\n')
+############################################################################################################
 
-def message_redditor(redditor, subreddit, subject=subject, message_template=message_template):
-    try:
-        message = message_template.replace('__SUBREDDIT__', subreddit)
-        redditor.message(subject=subject, message=message)
-    except Exception as e:
-        print('Unable to message redditor: ', str(e))
-
-
-def check_api_limits():
-    requests_remaining = reddit.auth.limits.get('remaining')
-    if requests_remaining > 0:
-        return
-    reset_datetime = datetime.fromtimestamp(reddit.auth.limits.get('reset_timestamp'))
-    current_datetime = datetime.now()
-    sleep_seconds = (reset_datetime - current_datetime).seconds
-    sleep(sleep_seconds)
-
-
-def message_submission_commenters(submission, subreddit, contacted_users, message_limit):
-    messages_sent = 0
-
-    title = submission.title.lower().split(' ')
-    title_words = set([word for word in title])
-
-    # Get comments if there is a keyword match
-    #############################################
-    if len(title_words.intersection(keywords)) >= 1:
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-
-            # Message user if not already contacted
-            ########################################
-            redditor = comment.author
-            if redditor not in contacted_users:
-                contacted_users.add(redditor.id)
-                message_redditor(redditor, subreddit)
-                messages_sent += 1
-
-                # 
-                ###########################
-                if messages_sent == message_limit:
-                    print('Hit message limit - exiting')
-                    return messages_sent
-
-SLEEP_TIME = 5
 start_time = perf_counter()
 messages_sent = 0
-for submission in reddit.subreddit("all").search(general_search_term, limit=20):
-    submission.comments.replace_more(limit=None)
+errors_seen = 0
 
-    # Add subreddit to list if not found
-    ####################################
-    subreddit = submission.subreddit
-    if subreddit not in subreddits:
-        subreddits.add(subreddit)
-    
-    for comment in submission.comments.list():
+# TODO: Add subreddit, submission IDs for posts crawled
+    # Can pick up process from subreddits/submissions not encountered in prior runs
+while messages_sent < message_limit:
+    print('Sending requests for search...')
+    for submission in reddit.subreddit("all").search(general_search_term, limit=20):
+        print('Changing comment replacement for submission...')
+        submission.comments.replace_more(limit=None)
 
-        # Add redditor to list if not found
+        # Add subreddit to list if not found
         ####################################
-        redditor = comment.author
-        redditor_id = redditor.id
-        if redditor not in redditors_contacted:
-            redditors_contacted.add(redditor_id)
-            
+        print('Getting subreddit...')
+        subreddit = submission.subreddit.display_name
+        print(f'Subreddit: {subreddit}')
+        if subreddit not in subreddits:
+            print(f'Found new subreddit to crawl: {subreddit}')
+            subreddits.add(subreddit)
+
+        print(f'Crawling comments for subreddit : {subreddit}')
+        for comment in submission.comments.list():
+
+            # Add redditor to list if not found
+            ####################################
+            try:
+                print('Fetching comment author...')
+                redditor    = comment.author
+                print(f'Comment author: {redditor}')
+                print('Fetching author ID...')
+                redditor_id = comment.author.id
+                print(f'Author ID: {redditor_id}')
+            except AttributeError:
+                continue
+
             # Message redditor / increment counters / check rate limits
             ############################################################
-            message_redditor(redditor, subreddit)
-            check_api_limits()
-            sleep(SLEEP_TIME)
+            if redditor_id not in redditors_contacted:
+                sleep(AUTHOR_SLEEP_TIME)
+                print(f'New redditor to message: {redditor_id}')
+                msg_status = message_redditor(redditor, subreddit)
 
-            messages_sent += 1
-            if messages_sent == message_limit:
+                # Update contacted redditors, error count based on output
+                ###########################################################
+                if msg_status:
+                    redditors_contacted.add(redditor_id)
+                    messages_sent += 1
+                    print('Checking rate limits...')
+                else:
+                    errors_seen += 1
 
-                # TODO: Write sets to files
+                # Check API limits, sleep, increment messages
+                #############################################
+                check_api_limits()
+                sleep(MESSAGE_SLEEP_TIME)
+                if messages_sent >= message_limit:
 
-                end_time = perf_counter()
-                total_runtime = end_time - start_time
-                total_seconds = round(total_runtime, 2)
-                total_minutes = round((end_time-start_time)/60, 2)
+                    # Write new redditors, subreddits to files
+                    #############################################
+                    write_data(redditors_contacted, 'redditors')
+                    write_data(subreddits, 'subreddits')
 
-                print(f'Total runtime (seconds): {total_seconds}')
-                print(f'Total runtime (minutes): {total_minutes}')
-                sys.exit("Max number of messages sent - exiting program")
+                    end_time = perf_counter()
+                    total_runtime = end_time - start_time
+                    total_seconds = round(total_runtime, 2)
+                    total_minutes = round((end_time-start_time)/60, 2)
+
+                    print(f'Total runtime (seconds): {total_seconds}')
+                    print(f'Total runtime (minutes): {total_minutes}')
+                    print(f'Total errors seen: {errors_seen}')
+                    sys.exit("Delivered max messages")
 
 
+############################################################################################################
+# SCRAPER #2 - Going through individual subs and pulling comments by keyword matches
+    # TODO:
+        # - refactor duplicate logic into functions for above, below crawls
+        # - apply for other post types (e..g, hot, rising, gilded, top)
+############################################################################################################
+
+start_time = perf_counter()
+messages_sent = 0
+errors_seen = 0
+
+while messages_sent < message_limit:
+    for sub in subreddits:
+        if sub != '':
+
+            ####################
+            #
+            # HOT POSTS 
+            #
+            ####################
+            for submission in reddit.subreddit(sub).hot(limit=50):
+                title = submission.title.lower().split(' ')
+                title_words = set([word for word in title])
+                print(f'Title: {title}')
+                if len(title_words.intersection(keywords)) > 1:
+                    print(f'1+ keyword matches found in title: {title_words}')
+                    print('Changing comment replacement for submission...')
+                    submission.comments.replace_more(limit=None)
+
+                    # Add subreddit to list if not found
+                    ####################################
+                    subreddit = sub
+                    print(f'Crawling comments for subreddit : {subreddit}')
+                    for comment in submission.comments.list():
+
+                        # Add redditor to list if not found
+                        ####################################
+                        try:
+                            print('Fetching comment author...')
+                            redditor    = comment.author
+                            print(f'Comment author: {redditor}')
+                            print('Fetching author ID...')
+                            redditor_id = comment.author.id
+                            print(f'Author ID: {redditor_id}')
+                        except AttributeError:
+                            continue
+
+                        # Message redditor / increment counters / check rate limits
+                        ############################################################
+                        if redditor_id not in redditors_contacted:
+                            sleep(AUTHOR_SLEEP_TIME)
+                            print(f'New redditor to message: {redditor_id}')
+                            msg_status = message_redditor(redditor, subreddit)
+                            if msg_status:
+                                redditors_contacted.add(redditor_id)
+                                messages_sent += 1
+                                print(f'Redditors Contacted: {len(redditors_contacted)}')
+                            else:
+                                errors_seen += 1
+                                print(f'Errors seen: {errors_seen}')
+                            print('Checking rate limits...')
+                            check_api_limits()
+                            sleep(MESSAGE_SLEEP_TIME)
+
+                            if messages_sent >= message_limit:
+                                # Write new redditors, subreddits to files
+                                #############################################
+                                write_data(redditors_contacted, 'redditors')
+                                write_data(subreddits, 'subreddits')
+
+                                end_time = perf_counter()
+                                total_runtime = end_time - start_time
+                                total_seconds = round(total_runtime, 2)
+                                total_minutes = round((end_time-start_time)/60, 2)
+
+                                print(f'Total runtime (seconds): {total_seconds}')
+                                print(f'Total runtime (minutes): {total_minutes}')
+                                print(f'Total errors seen: {errors_seen}')
+                                sys.exit("Delivered max messages")
+ 
 # TODO: Add subreddit-specific keyword matching
 # for subreddit in subreddits:
 #     print(subreddit)
